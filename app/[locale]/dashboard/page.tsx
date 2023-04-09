4e2877d31c05600c659152cf8316a4c41f686f6d
Perrin <perrin.grandne@protonmail.com>
2023-04-10 00:18:46 +0200

    update nb of players in dashboard

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react"
 const Dashboard = () => {
     const [nbUsers, setNbUsers] = useState(0)
     const getUsers = async () => {
-        fetch('/api/dashboard')
+        await fetch('/api/dashboard')
             .then((res) => res.json())
             .then((res) => {
                 setNbUsers(res.nbUsers)
@@ -22,7 +22,7 @@ const Dashboard = () => {
             <h1 className="font-permarker text-center text-7xl">
                 Dashboard
             </h1>
-            <p className=" mt-5 text-center text-5xl">{nbUsers} players</p>
+            <p className="mt-5 text-center text-5xl">{nbUsers} players</p>
         </div>
     )
 }
